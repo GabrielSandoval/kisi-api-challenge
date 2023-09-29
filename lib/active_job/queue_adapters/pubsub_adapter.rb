c05f53375e35e917b0bbcfd55831294071456e00
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2023-09-29 22:08:02 +0800

    Update PubsubAdapter to enqueue jobs to GCP

--- lib/active_job/queue_adapters/pubsub_adapter.rb
+++ lib/active_job/queue_adapters/pubsub_adapter.rb
@@ -4,18 +4,19 @@ module ActiveJob
   module QueueAdapters
     class PubsubAdapter
       # Enqueue a job to be performed.
-      #
       # @param [ActiveJob::Base] job The job to be performed.
       def enqueue(job)
-        raise(NotImplementedError)
+        data = job.arguments[0]
+        Rails.logger.info "[PubsubAdapter] Enqueue: #{data}"
+
+        Pubsub.publish!(data)
       end
 
       # Enqueue a job to be performed at a certain time.
-      #
       # @param [ActiveJob::Base] job The job to be performed.
       # @param [Float] timestamp The time to perform the job.
       def enqueue_at(job, timestamp)
-        raise(NotImplementedError)
+        raise NotImplementedError, "Check gcloud scheduler jobs create pubsub"
       end
     end
   end

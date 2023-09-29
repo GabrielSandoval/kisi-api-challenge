4f1e0205236a896b49c9f30757a1af6bd9f5d1e5
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2023-09-29 23:23:49 +0800

    Call Base.execute(job.serialize) to handle errors and retries

--- app/jobs/pubsub_job.rb
+++ app/jobs/pubsub_job.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class PubsubJob < ApplicationJob
-  class CustomError < RuntimeError; end
-
   queue_as :default
-  retry_on CustomError, wait: 5.minutes, attempts: 4
+  retry_on StandardError, wait: 5.seconds, attempts: 3
 
   self.queue_adapter = :pubsub
 
@@ -19,7 +17,7 @@ class PubsubJob < ApplicationJob
     # If it's already the 4th time, meaning the first,
     # second, and third try failed. Then push this
     # to :morgue queue
-    if job.executions >= 3 && job.queue_name != :morgue
+    if job.executions >= 2 && job.queue_name != :morgue
       job.queue_name = :morgue
     end
   end
--- lib/active_job/queue_adapters/pubsub_adapter.rb
+++ lib/active_job/queue_adapters/pubsub_adapter.rb
@@ -6,25 +6,21 @@ module ActiveJob
       # Enqueue a job to be performed.
       # @param [ActiveJob::Base] job The job to be performed.
       def enqueue(job)
-        data = job.arguments[0]
-        Rails.logger.info "[PubsubAdapter] Enqueue: #{data}"
-
-        Pubsub.publish!(data)
+        Rails.logger.info "[PubsubAdapter] Enqueue: #{job.arguments[0]}"
+        Base.execute(job.serialize)
       end
 
       # Enqueue a job to be performed at a certain time.
       # @param [ActiveJob::Base] job The job to be performed.
       # @param [Float] timestamp The time to perform the job.
       def enqueue_at(job, timestamp)
-        data = job.arguments[0]
-        Rails.logger.info "[PubsubAdapter] Enqueue At: #{data} - #{Time.at(timestamp)}"
-
+        Rails.logger.info "[PubsubAdapter] Enqueue At: #{job.arguments[0]} - #{Time.at(timestamp)}"
         delay = timestamp - Time.current.to_f
 
         if delay > 0
-          Concurrent::ScheduledTask.execute(delay) { Pubsub.publish!(data) }
+          Concurrent::ScheduledTask.execute(delay) { Base.execute(job.serialize) }
         else
-          Pubsub.publish!(data)
+          Base.execute(job.serialize)
         end
       end
     end
